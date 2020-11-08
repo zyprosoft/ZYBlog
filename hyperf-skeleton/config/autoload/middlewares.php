62d4434b72c9f4751eb8d736d20563a0a1a9c8c7
zyvincenthu <zyvincenthu@tencent.com>
2020-11-08 21:19:11 +0800

    update

--- hyperf-skeleton/config/autoload/middlewares.php
+++ hyperf-skeleton/config/autoload/middlewares.php
@@ -9,27 +9,13 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-if (env('APP_ENV') == 'dev') {
-    return [
-        'http' => [
-            ZYProSoft\Middleware\ValidatePostSizeMiddleware::class,
-            Hyperf\Session\Middleware\SessionMiddleware::class,
-            ZYProSoft\Middleware\CrossOriginMiddleware::class,
-            ZYProSoft\Middleware\RequestLimitMiddleware::class,
-            ZYProSoft\Middleware\RequestAuthMiddleware::class,
-            Hyperf\Validation\Middleware\ValidationMiddleware::class,
-        ],
-    ];
-}else{
-    return [
-        'http' => [
-            Hyperf\Metric\Middleware\MetricMiddleware::class,
-            ZYProSoft\Middleware\ValidatePostSizeMiddleware::class,
-            Hyperf\Session\Middleware\SessionMiddleware::class,
-            ZYProSoft\Middleware\CrossOriginMiddleware::class,
-            ZYProSoft\Middleware\RequestLimitMiddleware::class,
-            ZYProSoft\Middleware\RequestAuthMiddleware::class,
-            Hyperf\Validation\Middleware\ValidationMiddleware::class,
-        ],
-    ];
-}
+return [
+    'http' => [
+        ZYProSoft\Middleware\ValidatePostSizeMiddleware::class,
+        Hyperf\Session\Middleware\SessionMiddleware::class,
+        ZYProSoft\Middleware\CrossOriginMiddleware::class,
+        ZYProSoft\Middleware\RequestLimitMiddleware::class,
+        ZYProSoft\Middleware\RequestAuthMiddleware::class,
+        Hyperf\Validation\Middleware\ValidationMiddleware::class,
+    ],
+];
