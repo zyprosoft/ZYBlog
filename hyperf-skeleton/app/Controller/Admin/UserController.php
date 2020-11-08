67dc6991ea61b429d5f0b32522c4db3c29234abe
zyvincenthu <zyvincenthu@tencent.com>
2020-11-08 21:10:54 +0800

    update

--- hyperf-skeleton/app/Controller/Admin/UserController.php
+++ hyperf-skeleton/app/Controller/Admin/UserController.php
@@ -7,7 +7,13 @@ namespace App\Controller\Admin;
 use ZYProSoft\Controller\AbstractController;
 use App\Service\UserService;
 use Hyperf\Di\Annotation\Inject;
+use Hyperf\HttpServer\Annotation\AutoController;
 
+/**
+ * @AutoController(prefix='/admin/user')
+ * Class UserController
+ * @package App\Controller\Admin
+ */
 class UserController extends AbstractController
 {
 
