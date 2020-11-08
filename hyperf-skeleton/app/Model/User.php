60eeb076f2a6a02d070f0fc91854314e80aae118
zyvincent <1003081775@qq.com>
2020-11-08 16:16:13 +0800

    init project

---
+++ hyperf-skeleton/app/Model/User.php
@@ -0,0 +1,73 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model;
+
+use Hyperf\Database\Model\Events\Creating;
+use Hyperf\DbConnection\Model\Model;
+use Qbhy\HyperfAuth\Authenticatable;
+use App\Constants\Constants;
+
+/**
+ * @property int $user_id 自增ID
+ * @property int $role_id 角色ID
+ * @property string $username 账号
+ * @property string $nickname 昵称
+ * @property string $password 密码
+ * @property string $mobile 手机号
+ * @property string $avatar 头像
+ * @property int $approved 注册是否审核通过
+ * @property string $reason 驳回原因
+ * @property string $deleted_at 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ */
+class User extends Model implements Authenticatable
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['user_id' => 'integer', 'role_id' => 'integer', 'approved' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+
+    protected $hidden = ['password'];
+
+    public function getId()
+    {
+        return $this->user_id;
+    }
+
+    public static function retrieveById($key) : ?Authenticatable
+    {
+        $user = User::query()->select(["user_id","role_id","nickname","username","mobile","avatar","approved","deleted_at"])
+            ->where("userId", $key)
+            ->first();
+        if ($user instanceof Authenticatable) {
+            return $user;
+        }
+        return null;
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Verify::class);
+    }
+
+    public function isAdmin()
+    {
+        return $this->role_id == Constants::USER_ROLE_ADMIN;
+    }
+}
---
+++ hyperf-skeleton/config/autoload/hyperf-common.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'cgw' => [
+        'force_auth' => env('FORCE_AUTH', false),//强制校验签名,开启后CGW协议必须带签名参数访问
+        'config_list' => [
+            "test" => "abcdefg",
+        ]
+    ],
+    'rate_limit' => [
+        'access_rate_limit' => 10, //每分钟访问限制次数
+        'white_list' => [
+            '/weixin'
+        ],
+    ],
+    'cors' => [
+        'enable_cross_origin' => env('ENABLE_CROSS_ORIGIN', true),
+        'allow_cross_origins' => [
+            'http://127.0.0.1',
+            'http://localhost',
+            'http://lulinggushi.com'
+        ],
+    ],
+];
