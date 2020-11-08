43b467874823dc05518bd337d80450792452f6dc
zyvincenthu <zyvincenthu@tencent.com>
2020-11-08 22:54:51 +0800

    update

--- hyperf-skeleton/app/Controller/Admin/UserController.php
+++ hyperf-skeleton/app/Controller/Admin/UserController.php
@@ -4,9 +4,9 @@
 namespace App\Controller\Admin;
 
 
-use Hyperf\Contract\ContainerInterface;
 use ZYProSoft\Controller\AbstractController;
 use App\Service\UserService;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\AutoController;
 
 /**
@@ -17,24 +17,19 @@ use Hyperf\HttpServer\Annotation\AutoController;
 class UserController extends AbstractController
 {
     /**
+     * @Inject
      * @var UserService
      */
     private $userService;
 
-    public function __construct(ContainerInterface $container)
-    {
-        parent::__construct($container);
-        $this->userService = $container->get(UserService::class);
-    }
-
     public function login()
     {
-       $params = $this->validate([
-            'username' => 'string|max:20|exist:user,username|required',
-            'password' => 'string|max:20|required',
-        ]);
-        $username = $params['username'];
-        $password = $params['password'];
+//       $params = $this->validate([
+//            'username' => 'string|max:20|exist:user,username|required',
+//            'password' => 'string|max:20|required',
+//        ]);
+        $username = $this->request->param('username');
+        $password = $this->request->param('password');
         $userInfo = $this->userService->login($username, $password);
         return $this->success($userInfo);
     }
