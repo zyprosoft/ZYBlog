3bf82a9d4c310cefb27b0480397ff2f92fe97b2f
zyvincenthu <zyvincenthu@tencent.com>
2020-11-08 17:23:16 +0800

    fix seeder

--- hyperf-skeleton/seeders/create_user_role_data_seeder.php
+++ hyperf-skeleton/seeders/create_user_role_data_seeder.php
@@ -15,7 +15,7 @@ class CreateUserRoleDataSeeder extends Seeder
     public function run()
     {
         //
-        Db::table('user_role')->updateOrInsert(["role_id","name"],[["1","管理员"]]);
+        Db::table('user_role')->updateOrInsert(["role_id","name"],[[1,"管理员"]]);
         Db::table('category')->updateOrInsert(["name"],["经验分享","生活随笔","转载收藏"]);
     }
 }
