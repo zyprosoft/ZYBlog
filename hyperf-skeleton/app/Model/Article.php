4075ce6f48013b4f6ab98ee04026e547644e82e2
zyprosoft <1003081775@qq.com>
2020-12-30 19:51:01 +0800

    udpate

--- hyperf-skeleton/app/Model/Article.php
+++ hyperf-skeleton/app/Model/Article.php
@@ -41,7 +41,7 @@ class Article extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class,'article_tag','article_id','tag_id')->withTimestamps();
+        return $this->hasMany(Tag::class,'article_tag','article_id','article_id')->withTimestamps();
     }
 
     public function author()
