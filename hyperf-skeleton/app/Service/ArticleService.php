a634826ca2083f148589d6006ad79a5ecb2ba130
zyprosoft <1003081775@qq.com>
2020-12-31 11:01:36 +0800

    udpate

--- hyperf-skeleton/app/Service/ArticleService.php
+++ hyperf-skeleton/app/Service/ArticleService.php
@@ -49,7 +49,7 @@ class ArticleService extends BaseService
             if (isset($categoryId)) {
                 $query->where('category_id', $categoryId);
             }
-        })->offset($pageIndex * $pageSize)->limit($pageSize)->with(['author','tags','category'])->get();
+        })->orderByDesc('updated_at')->offset($pageIndex * $pageSize)->limit($pageSize)->with(['author','tags','category'])->get();
 
        $total = Article::count('*');
 
