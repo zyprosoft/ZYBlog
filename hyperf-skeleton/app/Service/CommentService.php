3cd1328d8b28dee146ead672e1bbda9b977a733f
zyprosoft <1003081775@qq.com>
2021-01-23 12:25:29 +0800

    update

--- hyperf-skeleton/app/Service/CommentService.php
+++ hyperf-skeleton/app/Service/CommentService.php
@@ -27,6 +27,7 @@ class CommentService extends BaseService
         }
         Log::info("will save comment:".$comment->toJson());
         $comment->saveOrFail();
+        Log::info("after save comment:".$comment->toJson());
         $this->push(new RefreshArticleJob($articleId));
         //清空这个文章的评论缓存
         $this->clearListCacheWithMaxPage('CommentListForEach', [$articleId], $this->clearListPageSize);
