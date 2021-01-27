465b497f558418fec17930d6a4be550f85ed882e
zyprosoft <1003081775@qq.com>
2021-01-27 21:39:15 +0800

    update

--- hyperf-skeleton/config/autoload/crontab.php
+++ hyperf-skeleton/config/autoload/crontab.php
@@ -1,6 +1,14 @@
 <?php
+use Hyperf\Crontab\Crontab;
+use ZYProSoft\Task\ClearExpireCaptchaTask;
 
 return [
     // 是否开启定时任务
     'enable' => true,
+    'crontab' => [
+        (new Crontab())->setName('clearCaptcha')
+                       ->setRule('*/10 * * * *')
+                       ->setCallback([ClearExpireCaptchaTask::class, 'execute'])
+                       ->setMemo('定时清除过期的验证码'),
+    ]
 ];
