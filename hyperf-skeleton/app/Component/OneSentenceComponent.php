3931b35c72c7b394a39ae96a43f3887aef5ef61b
zyprosoft <1003081775@qq.com>
2021-02-01 22:43:47 +0800

    update

---
+++ hyperf-skeleton/app/Component/OneSentenceComponent.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Component;
+use Hyperf\Utils\Arr;
+use ZYProSoft\Component\BaseComponent;
+
+class OneSentenceComponent extends BaseComponent
+{
+    protected $options = [
+        "base_uri" => "http://api.youngam.cn/",
+        "timeout" => 3,
+    ];
+
+    const INTERFACE_GET_SENTENCE = "api/one.php";
+
+    public function getOneSentence()
+    {
+        $result = $this->get(self::INTERFACE_GET_SENTENCE);
+        if ($result->getStatusCode() != 200) {
+            return  $this->success();
+        }
+        $sentence = Arr::get(json_decode($result->getBody(), true), 'data.text');
+
+        return $this->success($sentence);
+    }
+}
