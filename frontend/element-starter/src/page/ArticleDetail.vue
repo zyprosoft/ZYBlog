2ccc918403f1cae68cfe7d06f6077a88de1cdae8
zyprosoft <1003081775@qq.com>
2021-02-16 15:03:35 +0800

    update

--- frontend/element-starter/src/page/ArticleDetail.vue
+++ frontend/element-starter/src/page/ArticleDetail.vue
@@ -224,9 +224,6 @@ export default {
     props: {
 
     },
-    activated() {
-        this.setGoIndex()
-    },
     watch: {
         $route(to, from) {
             console.log('to params:' + JSON.stringify(to.params))
@@ -290,21 +287,17 @@ export default {
                 this.isCommentPosting = false;
             });
         },
-        setGoIndex() {
-            if (window.history.length <= 1) {
-                if (location.href.indexOf('?') === -1) {
-                    window.location.href = location.href + '?goindex=true'
-                } else if (location.href.indexOf('?') !== -1 && location.href.indexOf('goindex') === -1) {
-                    window.location.href = location.href + '&goindex=true'
-                }
-            }
-        },
         hanldeGoBack() {
-            if (this.$route.query.goindex === 'true') {
+            if (window.history.length <= 1) {
                 this.$router.push({ name: 'index' })
             } else {
                 this.$router.back(-1)
             }
+            //上面都没执行就说明卡在当前页不是最后一条， histroy记录数量大于1，又没有回退记录，只能返回首页，
+            //如果上面都执行了 页面都跳走了，这个也就不用管了   
+            setTimeout(() => {
+                this.$router.push({ name: 'index' })
+            }, 500)
         },
         refreshArticleDetail() {
             if (this.articleId == null) {
