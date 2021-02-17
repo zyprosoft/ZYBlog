ca67d932c8aad19a7d37c0fd2a2d1a91fd4f98c6
zyprosoft <1003081775@qq.com>
2021-02-17 14:33:55 +0800

    Update server.php

--- hyperf-skeleton/config/autoload/server.php
+++ hyperf-skeleton/config/autoload/server.php
@@ -13,7 +13,7 @@ use Hyperf\Server\Server;
 use Hyperf\Server\Event;
 
 return [
-    'mode' => SWOOLE_IPC_PREEMPTIVE,
+    'mode' => SWOOLE_BASE,
     'servers' => [
         [
             'name' => 'http',
@@ -36,6 +36,7 @@ return [
         'max_request' => 100000,
         'socket_buffer_size' => 2 * 1024 * 1024,
         'buffer_output_size' => 2 * 1024 * 1024,
+        'dispatch_mode' => 3, //抢占模式
         // 将 public 替换为上传目录
         'document_root' => BASE_PATH . '/public',
         'enable_static_handler' => true, //走nginx直接代理访问这个公开目录就好了
